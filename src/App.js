465322d6ea6302d56eabc601e4e5bf5b417cafda
soonji <youinxxs@naver.com>
2022-09-21 10:15:48 +0900

    서해종합건설 SPA

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='Wrap'>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
