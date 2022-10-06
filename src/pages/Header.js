57d490374ddcb61cf493ece25649bd91819ce3db
soonji <youinxxs@naver.com>
2022-10-06 16:31:14 +0900

    20221006

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,19 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Navmenu from './Navmenu'
-
-
-// on 이벤트 https://goddino.tistory.com/317?category=1080917, https://velog.io/@devstefancho/react-scroll-event-5e1vuub9
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Navmenu from './Navmenu';
+import '../css/basic.scss';
+// on 이벤트 https://goddino.tistory.com/317?category=1080917,
+// https://velog.io/@devstefancho/react-scroll-event-5e1vuub9
 
 
 const Header = () => {
+    const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장
+    const [ScrollActive, setScrollActive] = useState(false);
+    function handleScroll() {
+        { console.log(ScrollY) }
+        if (ScrollY > 500) {
+            setScrollY(window.pageYOffset);
+            setScrollActive(true);
+        } else {
+            setScrollY(window.pageYOffset);
+            setScrollActive(false);
+        }
+    }
+    useEffect(() => {
+        function scrollListener() {
+            window.addEventListener("scroll", handleScroll);
+        } //  window 에서 스크롤을 감시 시작
+        scrollListener(); // window 에서 스크롤을 감시
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }; //  window 에서 스크롤을 감시를 종료
+    });
+
     return (
-        <header className='Header'>
+        <header className={ScrollActive ? "Header fixed" : "Header"}>
             <div className="container">
                 <Link to="/adm">
                     <h1 className='logo'></h1>
-                </Link>
-                <nav>
+                </Link><nav>
                     <Navmenu />
                 </nav>
                 <i className='xi-bars'></i>
